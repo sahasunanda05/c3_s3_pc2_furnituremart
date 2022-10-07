3ee6fda503784d29f87d223e3723c1a0f4ea0dc7
SUNANDA SAHA <sahasunanda73@gmail.com>
2022-10-07 14:41:48 +0530

    solution added

--- src/com/furniture/FunitureItemImpl.java
+++ src/com/furniture/FunitureItemImpl.java
@@ -2,19 +2,19 @@ package com.furniture;
 
 public class FunitureItemImpl {
     public static void main(String[] args) {
-        FurnitureItem obj = new FurnitureItem();
-        obj.setFurnitureCode(101);
-        System.out.println("Furniture Code = " + obj.getFurnitureCode());
-        obj.setFurnitureType("table");
-        System.out.println("Furniture Type = " + obj.getFurnitureType());
-        obj.setGradeOfFurniture("grade1");
-        System.out.println("gradeOfFurniture = " + obj.getGradeOfFurniture());
-        obj.setFurnitureUsage("outdoor");
-        System.out.println("getFurnitureUsage = " + obj.getFurnitureUsage());
-        obj.setPrice(10);
-        System.out.println("price = $" + obj.getPrice());
+        FurnitureItem furnitureItem = new FurnitureItem();
+        furnitureItem.setFurnitureCode(101);
+        System.out.println("Furniture Code = " + furnitureItem.getFurnitureCode());
+        furnitureItem.setFurnitureType("table");
+        System.out.println("Furniture Type = " + furnitureItem.getFurnitureType());
+        furnitureItem.setGradeOfFurniture("grade1");
+        System.out.println("gradeOfFurniture = " + furnitureItem.getGradeOfFurniture());
+        furnitureItem.setFurnitureUsage("outdoor");
+        System.out.println("getFurnitureUsage = " + furnitureItem.getFurnitureUsage());
+        furnitureItem.setPrice(10);
+        System.out.println("price = $" + furnitureItem.getPrice());
 
-        System.out.println("Discounted Price is = $" + obj.calculateDiscount());
+        System.out.println("Discounted Price is = $" + furnitureItem.calculateDiscount());
 
 
     }
